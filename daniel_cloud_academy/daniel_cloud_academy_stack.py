9a888eca8d17a86962af4315ab4686499830fc81
Ubuntu <ubuntu@ip-172-31-33-110.eu-north-1.compute.internal>
2023-08-16 21:45:47 +0000

    reformat tack

--- daniel_cloud_academy/daniel_cloud_academy_stack.py
+++ daniel_cloud_academy/daniel_cloud_academy_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+
 class DanielCloudAcademyStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
