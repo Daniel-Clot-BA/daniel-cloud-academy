f9c1626553fdd5fcc589dc9adcb4e282f8f9f153
Daniel Clot <daniel.clot@ba.com>
2023-08-17 14:00:30 +0100

    full project, hopefully works

---
+++ daniel_cloud_academy/daniel_cloud_academy_stack.py
@@ -0,0 +1,21 @@
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3
+)
+from constructs import Construct
+
+
+class DanielCloudAcademyStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # my resources
+
+        daniels_bucket = s3.Bucket(
+            self,
+            'daniels-bucket',
+            versioned=True,
+            encryption=s3.BucketEncryption.KMS,
+            bucket_key_enabled=True,
+        )
